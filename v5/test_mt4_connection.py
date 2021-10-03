c8cd1619b483c97f3d6c400437e3efcc74f1515b
Chris Evangelio <cevangelio@users.noreply.github.com>
2021-10-03 17:38:11 +0800

    test

--- v5/test_mt4_connection.py
+++ v5/test_mt4_connection.py
@@ -2,8 +2,8 @@
 
 from Pytrader_API_V1_06 import *
 MT = Pytrader_API()
-port = [1122, 1125, 1127] #FXCM MAIN 50k 1:100
-port_dict = {1122:'FTMO', 1125:'FXCM', 1127:'Global_Prime'}
+port = [1122, 1125, 1127, 1129, 1131] #FXCM MAIN 50k 1:100
+port_dict = {1122:'FTMO_Live', 1125:'FXCM_DEMO', 1127:'GP_Live', 1129: 'GP_DEMO', 1131:'FTMO_DEMO'}
 list_symbols = ['AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURUSD', 'CHFJPY', 'GBPAUD', 'GBPCHF', 'GBPJPY', 'GBPUSD', 'NZDCAD', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY']
 symbols = {}
 for pair in list_symbols:
@@ -15,3 +15,5 @@ for mt4 in port:
         pera = MT.Get_dynamic_account_info()['balance']
         positions = MT.Get_all_open_positions()
         print(port_dict[mt4], 'MT4 connection is working. Your current balance is', str(pera), 'with' , len(positions), 'total positions open.')
+    else:
+        print(port_dict[mt4], 'Not working')
